25999d96207303688d23b11508add8398c923a8e
Damiano Ravalico <damianoravalico.uni@gmail.com>
2023-07-04 10:19:20 +0200

    Initial commit

---
+++ app/src/main/java/it/units/sim/bookmarkhub/MainActivity.java
@@ -0,0 +1,14 @@
+package it.units.sim.bookmarkhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
