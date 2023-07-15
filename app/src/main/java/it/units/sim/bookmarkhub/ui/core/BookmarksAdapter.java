d645251026cb69e28d3dfcb629c35ba0047c5c1c
Damiano Ravalico <damianoravalico.uni@gmail.com>
2023-07-15 11:08:58 +0200

    Now you have the favicon of the bookmark site

--- app/src/main/java/it/units/sim/bookmarkhub/ui/core/BookmarksAdapter.java
+++ app/src/main/java/it/units/sim/bookmarkhub/ui/core/BookmarksAdapter.java
@@ -5,14 +5,18 @@ import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.browser.customtabs.CustomTabsIntent;
 import androidx.cardview.widget.CardView;
-import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 
@@ -31,6 +35,18 @@ public class BookmarksAdapter extends FirestoreRecyclerAdapter<Bookmark, Bookmar
     protected void onBindViewHolder(@NonNull BookmarkViewHolder holder, int position, @NonNull Bookmark model) {
         holder.name.setText(model.name);
         holder.data.setText(model.data);
+        Uri uri = Uri.parse(model.url);
+        String scheme = uri.getScheme();
+        String host = uri.getHost();
+        Glide.with(activity)
+                .asBitmap()
+                .load(scheme + "://" + host + "/favicon.ico")
+                .apply(new RequestOptions()
+                        .override(Target.SIZE_ORIGINAL)
+                        .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+                        .skipMemoryCache(false))
+                .error(R.drawable.image_not_supported)
+                .into(holder.favicon);
         holder.cardView.setOnClickListener(v ->
                 new CustomTabsIntent.Builder().build().launchUrl(activity, Uri.parse(model.url))
         );
@@ -47,12 +63,14 @@ public class BookmarksAdapter extends FirestoreRecyclerAdapter<Bookmark, Bookmar
         public final CardView cardView;
         public final TextView name;
         public final TextView data;
+        public final ImageView favicon;
 
         BookmarkViewHolder(@NonNull View itemView) {
             super(itemView);
             cardView = itemView.findViewById(R.id.card);
             name = itemView.findViewById(R.id.name);
             data = itemView.findViewById(R.id.data);
+            favicon = itemView.findViewById(R.id.faviconImageView);
         }
     }
 
