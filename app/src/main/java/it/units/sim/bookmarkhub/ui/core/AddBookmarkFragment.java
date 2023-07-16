edc1dfa8003b3f338dd759d1f8f74c46d0a2d7b3
Damiano Ravalico <damianoravalico.uni@gmail.com>
2023-07-16 18:11:27 +0200

    Updated the max value for bookmark name and addtional data

--- app/src/main/java/it/units/sim/bookmarkhub/ui/core/AddBookmarkFragment.java
+++ app/src/main/java/it/units/sim/bookmarkhub/ui/core/AddBookmarkFragment.java
@@ -45,12 +45,12 @@ public class AddBookmarkFragment extends Fragment {
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-            int maxNameLength = 25;
+            int maxNameLength = 30;
             if (nameEditText.getText().toString().length() > maxNameLength) {
                 nameEditText.setText(s.subSequence(0, maxNameLength));
                 nameEditText.setSelection(maxNameLength);
             }
-            int maxDataLength = 40;
+            int maxDataLength = 50;
             if (dataEditText.getText().toString().length() > maxDataLength) {
                 dataEditText.setText(s.subSequence(0, maxDataLength));
                 dataEditText.setSelection(maxDataLength);
